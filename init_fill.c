919ee1a4bd32584b8aaa2a3600e77c9595b683da
Wongoose <wong.zhengxiang@gmail.com>
2022-05-29 10:44:55 +0800

    Arranged folder structure (helper & resources)

--- init_fill.c
+++ init_fill.c
@@ -2,22 +2,20 @@
 
 char	*ft_strcpy(char *dest, char *src);
 
-void	init_fill(char board[4][4][5], int row_size, int col_size)
+void	init_fill(char board[4][4][5], int size)
 {
 	int		row_i;
 	int		col_i;
+	int		index;
 
 	row_i = 0;
 	col_i = 0;
-	while (row_i < row_size)
+	index = 0;
+	while (index < size*size)
 	{
-		col_i = 0;
-		while (col_i < col_size)
-		{
-			ft_strcpy(board[row_i][col_i], "1234");
-			//printf("Copied this: %s\n", board[row_i][col_i]);
-			col_i++;
-		}
-		row_i++;
+		row_i = index / size;
+		col_i = index % size;
+		ft_strcpy(board[row_i][col_i], "1234");
+		index++;
 	}
 }
