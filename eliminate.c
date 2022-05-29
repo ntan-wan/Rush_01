2dcec07a3f39fad649b4284afbf1a3ca2ea661c0
Wongoose <wong.zhengxiang@gmail.com>
2022-05-29 10:55:29 +0800

    Updated eliminate.c with 0-15 index

--- eliminate.c
+++ eliminate.c
@@ -1,43 +1,41 @@
-void	eliminate_row(char board[4][4][5], int pos_row, int size, char to_remove);
-void	eliminate_col(char board[4][4][5], int pos_col, int size, char to_remove);
+void	eliminate_row(char board[4][4][5], int index, char to_remove);
+void	eliminate_col(char board[4][4][5], int index, char to_remove);
 char	*ft_strcpy(char *dest, char *src);
 char	*remove_char(char *str, char to_remove);
 
-void	eliminate(char board[4][4][5], int pos_row, int pos_col, int size, char to_remove)
+void	eliminate(char board[4][4][5], int index, char to_remove)
 {
-	int	row_i;
-	int	col_i;
-
-	row_i = 0;
-	col_i = 0;
-
-	eliminate_row(board, pos_row, size, to_remove);
-	eliminate_col(board, pos_col, size, to_remove);
+	eliminate_row(board, index, to_remove);
+	eliminate_col(board, index, to_remove);
 }
 
-void	eliminate_row(char board[4][4][5], int pos_row, int size, char to_remove)
+void	eliminate_row(char board[4][4][5], int index, char to_remove)
 {
-	int row_i;
-	int	col_i;
+	int 	row_i;
+	int		col_i;
+	char	*field;
 
-	row_i = pos_row;
+	row_i = index / 4;
 	col_i = 0;
-	while (col_i < size)
+	while (col_i < 4)
 	{
-		ft_strcpy(board[row_i][col_i], remove_char(board[row_i][col_i], to_remove));
+		field = &*board[row_i][col_i];
+		ft_strcpy(field, remove_char(board[row_i][col_i], to_remove));
 		col_i++;
 	}
 }
 
-void	eliminate_col(char board[4][4][5], int pos_col, int size, char to_remove)
+void	eliminate_col(char board[4][4][5], int index, char to_remove)
 {
-	int row_i;
-	int	col_i;
+	int 	row_i;
+	int		col_i;
+	char	*field;
 
 	row_i = 0;
-	col_i = pos_col;
-	while (row_i < size)
+	col_i = index % 4;
+	while (row_i < 4)
 	{
+		field = &*board[row_i][col_i];
 		ft_strcpy(board[row_i][col_i], remove_char(board[row_i][col_i], to_remove));
 		row_i++;
 	}
