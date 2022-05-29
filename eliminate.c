70ce3b6436785d2f191723190bbc7726af603c79
Wongoose <wong.zhengxiang@gmail.com>
2022-05-29 11:07:33 +0800

    Updated eliminate with OK norminette

--- eliminate.c
+++ eliminate.c
@@ -1,5 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   eliminate.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zwong <zwong@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/29 11:06:14 by zwong             #+#    #+#             */
+/*   Updated: 2022/05/29 11:07:04 by zwong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 void	eliminate_row(char board[4][4][5], int index, char to_remove);
 void	eliminate_col(char board[4][4][5], int index, char to_remove);
+void	insert_field(char board[4][4][5], int index, char to_remove);
 char	*ft_strcpy(char *dest, char *src);
 char	*remove_char(char *str, char to_remove);
 
@@ -7,11 +20,12 @@ void	eliminate(char board[4][4][5], int index, char to_remove)
 {
 	eliminate_row(board, index, to_remove);
 	eliminate_col(board, index, to_remove);
+	insert_field(board, index, to_remove);
 }
 
 void	eliminate_row(char board[4][4][5], int index, char to_remove)
 {
-	int 	row_i;
+	int		row_i;
 	int		col_i;
 	char	*field;
 
@@ -20,14 +34,14 @@ void	eliminate_row(char board[4][4][5], int index, char to_remove)
 	while (col_i < 4)
 	{
 		field = &*board[row_i][col_i];
-		ft_strcpy(field, remove_char(board[row_i][col_i], to_remove));
+		ft_strcpy(field, remove_char(field, to_remove));
 		col_i++;
 	}
 }
 
 void	eliminate_col(char board[4][4][5], int index, char to_remove)
 {
-	int 	row_i;
+	int		row_i;
 	int		col_i;
 	char	*field;
 
@@ -36,7 +50,19 @@ void	eliminate_col(char board[4][4][5], int index, char to_remove)
 	while (row_i < 4)
 	{
 		field = &*board[row_i][col_i];
-		ft_strcpy(board[row_i][col_i], remove_char(board[row_i][col_i], to_remove));
+		ft_strcpy(field, remove_char(field, to_remove));
 		row_i++;
 	}
+}
+
+void	insert_field(char board[4][4][5], int index, char to_remove)
+{
+	int		row_i;
+	int		col_i;
+	char	*field;
+
+	row_i = index / 4;
+	col_i = index % 4;
+	field = &*board[row_i][col_i];
+	ft_strcpy(field, &to_remove);
 }
